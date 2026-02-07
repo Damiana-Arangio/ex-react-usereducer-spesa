9a89be48330a7d20bbfa4d5978a9079d8fc03df4
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-07 23:27:27 +0100

    feat(milestone 1): lista prodotti leggibile con nome e prezzo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import './App.css'
 
 function App() {
 
+  /***************
+      COSTANTI 
+  ****************/ 
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  /************
+     RENDER 
+ **************/ 
   return (
     <>
+      <h1> Lista prodotti</h1>
+      <ul>
+        {products.map( (product, index) => (
+            <li key={index}>
+              {product.name} – {product.price.toFixed(2)} €
+            </li>
+        ))}
+      
+      </ul>
     </>
   )
 }
