103bcaefb0a15e2c8ba699806101c89fcf044a3d
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-08 22:59:26 +0100

    feat(milestone 3): gestione aggiunta, rimozione e prezzo totale del carrello

--- src/App.jsx
+++ src/App.jsx
@@ -17,26 +17,24 @@ function App() {
       HOOK 
   ************/ 
   const [addedProducts, setAddedProducts] = useState([]);   // Stato che rappresenta i prodotti nel carrello.
-  console.log(addedProducts);
 
   /************
      RENDER 
   **************/ 
   return (
     <>
-
-    {/* Lista Prodotti */}
-    <div>
-        <h1> Lista prodotti</h1>
-        <ul>
-          {products.map((product, index) => (
-            <li key={index}>
-              {product.name} – {product.price.toFixed(2)} €
-              <button onClick={() => addToCart(product)}> Aggiungi al carrello </button>
-            </li>
-          ))}
-        </ul>
-    </div>
+      {/* Lista Prodotti */}
+      <div>
+          <h1> Lista prodotti</h1>
+          <ul>
+            {products.map((product, index) => (
+              <li key={index}>
+                {product.name} – {product.price.toFixed(2)} €
+                <button onClick={() => addToCart(product)}> Aggiungi al carrello </button>
+              </li>
+            ))}
+          </ul>
+      </div>
 
       {/* Carrello */}
       {addedProducts.length > 0 &&
@@ -46,9 +44,21 @@ function App() {
             {addedProducts.map((addedProduct, index) => (
               <li key={index}>
                 {addedProduct.name} – {addedProduct.price.toFixed(2)} € - x{addedProduct.quantity}
+                <button onClick={() => removeFromCart(addedProduct)}> Rimuovi dal carrello </button>
               </li>
             ))}
           </ul>
+
+          {/* Totale da pagare */}
+          <h3>
+            Totale: {
+              addedProducts.reduce((acc, currProduct) =>
+                acc + (currProduct.price * currProduct.quantity),
+                0
+              ).toFixed(2)
+            } €
+          </h3>
+          
         </div>
       }
     </>
@@ -58,10 +68,10 @@ function App() {
       FUNZIONI 
   ***************/ 
 
-  // Funzione per aggiungere i prodotti nel carrello
+  // Funzione per aggiungere un prodotto nel carrello
   function addToCart(product) {
 
-    // Se il prodotto non è gia nel carrello, lo aggiungo
+    // Se il prodotto non è presente nel carrello, lo aggiungo
     const isPresent = addedProducts.some( addedProduct => addedProduct.name === product.name);
     if(!isPresent) {
       setAddedProducts( currAddedProducts => [
@@ -69,7 +79,33 @@ function App() {
         {...product, quantity: 1 }
       ]);
     }
+    // Se il prodotto è gia presente, invoco la funzione per incrementare la quantità
+    else {
+      const arrayAggiornato = updateProductQuantity(product);
+      setAddedProducts(arrayAggiornato);
+    }
   }
-}
 
-export default App
+  // Funzione per incrementare la quantità di un prodotto già esistente nel carrello
+  function updateProductQuantity(product) {
+
+    const arrayAggiornato = addedProducts.map( prodottoCarrello => (
+      prodottoCarrello.name === product.name 
+      ? {...prodottoCarrello, quantity: prodottoCarrello.quantity + 1} 
+      : prodottoCarrello
+    ));
+
+    return arrayAggiornato;
+  }
+
+  // Funzione per rimuovere un prodotto dal carrello
+  function removeFromCart(addedProduct) {
+    
+    setAddedProducts ( currAddedProducts => 
+      currAddedProducts.filter(prodottoCarrello => prodottoCarrello.name !== addedProduct.name
+    ));
+  }
+
+}
+export default App;
+
