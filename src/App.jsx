00b63b1611a0026bf30d593e9fac9f080a401b02
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-08 17:28:00 +0100

    feat(milestone 2): gestione prodotti nel carrello

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
@@ -12,22 +13,63 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  /***********
+      HOOK 
+  ************/ 
+  const [addedProducts, setAddedProducts] = useState([]);   // Stato che rappresenta i prodotti nel carrello.
+  console.log(addedProducts);
+
   /************
      RENDER 
- **************/ 
+  **************/ 
   return (
     <>
-      <h1> Lista prodotti</h1>
-      <ul>
-        {products.map( (product, index) => (
+
+    {/* Lista Prodotti */}
+    <div>
+        <h1> Lista prodotti</h1>
+        <ul>
+          {products.map((product, index) => (
             <li key={index}>
               {product.name} – {product.price.toFixed(2)} €
+              <button onClick={() => addToCart(product)}> Aggiungi al carrello </button>
             </li>
-        ))}
-      
-      </ul>
+          ))}
+        </ul>
+    </div>
+
+      {/* Carrello */}
+      {addedProducts.length > 0 &&
+        <div>
+          <h2> Carrello </h2>
+          <ul>
+            {addedProducts.map((addedProduct, index) => (
+              <li key={index}>
+                {addedProduct.name} – {addedProduct.price.toFixed(2)} € - x{addedProduct.quantity}
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
     </>
   )
+
+  /**************
+      FUNZIONI 
+  ***************/ 
+
+  // Funzione per aggiungere i prodotti nel carrello
+  function addToCart(product) {
+
+    // Se il prodotto non è gia nel carrello, lo aggiungo
+    const isPresent = addedProducts.some( addedProduct => addedProduct.name === product.name);
+    if(!isPresent) {
+      setAddedProducts( currAddedProducts => [
+        ...currAddedProducts, 
+        {...product, quantity: 1 }
+      ]);
+    }
+  }
 }
 
 export default App
