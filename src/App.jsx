4cbad34e09192146f54599e426ebeaff4026a523
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-10 00:30:09 +0100

    feat(bonus 2): ottimizzato carrello con useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import './App.css'
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
   /***********
       HOOK 
   ************/ 
-  const [addedProducts, setAddedProducts] = useState([]);       // Stato che rappresenta i prodotti nel carrello.
+  const [cartProducts, dispatch] = useReducer(cartReducer, [])    // Stato complesso per gestire i prodotti nel carrello
   
   /************
      RENDER 
@@ -32,20 +32,20 @@ function App() {
                 {product.name} – {product.price.toFixed(2)} €
 
                 {/* Bottone per aggiungere un prodotto nel carrello */}
-                <button onClick={() => addToCart(product)}> Aggiungi al carrello </button>
+                <button onClick={ () => dispatch({ type: "ADD_ITEM", payload: product })}> Aggiungi al carrello </button>
               </li>
             ))}
           </ul>
       </div>
 
       {/* Carrello */}
-      {addedProducts.length > 0 &&
+      {cartProducts.length > 0 &&
         <div>
           <h2> Carrello </h2>
 
           {/* Lista prodotti carrello */}
           <ul>
-            {addedProducts.map((addedProduct, index) => (
+            {cartProducts.map((addedProduct, index) => (
               <li key={index}>
                 {addedProduct.name} – {addedProduct.price.toFixed(2)} €
 
@@ -55,11 +55,12 @@ function App() {
                   type="number" 
                   min={1}
                   value={addedProduct.quantity}
-                  onChange={(e) => updateProductQuantity(e, addedProduct)}
+                  onChange={(e) => dispatch({ type: "UPDATE_QUANTITY", payload: { addedProduct: addedProduct, newQuantity : parseInt(e.target.value)}})}
                 />
 
                 {/* Bottone per rimuovere un prodotto dal carrello */}
-                <button onClick={() => removeFromCart(addedProduct)}> Rimuovi dal carrello </button>
+                <button onClick={() => 
+                  dispatch( {type : "REMOVE_ITEM", payload : addedProduct} )}> Rimuovi dal carrello </button>
               </li>
             ))}
           </ul>
@@ -67,7 +68,7 @@ function App() {
           {/* Totale da pagare */}
           <h3>
             Totale: {
-              addedProducts.reduce((acc, currProduct) =>
+              cartProducts.reduce((acc, currProduct) =>
                 acc + (currProduct.price * currProduct.quantity),
                 0
               ).toFixed(2)
@@ -82,44 +83,62 @@ function App() {
       FUNZIONI 
   ***************/ 
 
-  // Funzione per aggiungere un prodotto nel carrello
-  function addToCart(product) {
+  // Funzione reducer 
+  function cartReducer(prodottiCarrello, action) {
+    switch (action.type) {
 
-    // Se il prodotto non è presente nel carrello, lo aggiungo (quantità = 1)
-    const isPresent = addedProducts.some( addedProduct => addedProduct.name === product.name);
-    if(!isPresent) {
-      setAddedProducts( currAddedProducts => [
-        ...currAddedProducts, 
-        { ...product, quantity: 1 }
-      ]);
+      case 'ADD_ITEM': {
+
+        const product = action.payload;
+          
+        // Se il prodotto è già presente nel carrello, incremento la quantità
+        const isPresent = prodottiCarrello.some(prodottoCarrello => prodottoCarrello.name === product.name);
+          if (isPresent) {
+            return prodottiCarrello.map(
+              prodottoCarrello => prodottoCarrello.name === product.name
+                ? { ...prodottoCarrello, quantity: prodottoCarrello.quantity + 1 }
+                : prodottoCarrello
+            );
+          }
+
+        // Se il prodotto non è presente nel carrello, lo aggiungo (quantità = 1)
+        return [...prodottiCarrello, { ...product, quantity: 1 }];
+      }
+
+
+      case 'REMOVE_ITEM': {
+
+        const addedProduct = action.payload
+
+        return prodottiCarrello.filter
+          (prodottoCarrello => prodottoCarrello.name !== addedProduct.name)
+      }
+          
+
+
+
+      case 'UPDATE_QUANTITY': {
+        const { addedProduct, newQuantity } = action.payload;
+
+        // Se l'input è un numero valido (>0) aggiorno lo stato
+        if (newQuantity > 0 && !isNaN(newQuantity)) {
+          return prodottiCarrello.map(prodottoCarrello =>
+            prodottoCarrello.name === addedProduct.name
+              ? { ...prodottoCarrello, quantity: newQuantity }
+              : prodottoCarrello
+          );
+        }
+
+        // Se la quantità non è valida, ritorno lo stato invariato
+        return prodottiCarrello;
+      }
+
+      default:
+        return prodottiCarrello;
     }
   }
-
-  // Funzione per aggiornare la quantità di un prodotto dal carrello
-  function updateProductQuantity(e, product) {
-
-    let newQuantity = parseInt(e.target.value);
-
-    // Se l'input è un numero valido (>0) aggiorno lo stato
-    if (newQuantity > 0 && !isNaN(newQuantity)) {
-    
-      setAddedProducts(currAddedProducts =>
-      currAddedProducts.map(prodottoCarrello =>
-        prodottoCarrello.name === product.name
-          ? { ...prodottoCarrello, quantity: newQuantity }
-          : prodottoCarrello
-      ))
-    }
-  }
-
-  // Funzione per rimuovere un prodotto dal carrello
-  function removeFromCart(addedProduct) {
-    
-    setAddedProducts ( currAddedProducts => 
-      currAddedProducts.filter(prodottoCarrello => prodottoCarrello.name !== addedProduct.name
-    ));
-  }
-
 }
+
 export default App;
+
 
